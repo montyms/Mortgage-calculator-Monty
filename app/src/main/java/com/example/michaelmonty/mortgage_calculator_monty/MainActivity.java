9df1f8091d3a815ef0b2baeaae4c7ba700bb606a
montyms <montyms@g.cofc.edu>
2017-02-07 18:27:53 -0500

    with screenshots

--- app/src/main/java/com/example/michaelmonty/mortgage_calculator_monty/MainActivity.java
+++ app/src/main/java/com/example/michaelmonty/mortgage_calculator_monty/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.michaelmonty.mortgage_calculator_monty;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mortSumButton;
+    private Button paySumButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
